d0c0c1b4a9c1dd7e4b84c5430ba30e81ea951efc
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-12-04 06:18:38 +0200

    last

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {loadState, saveState} from './getState';
 
 function App() {
 
-  const [goal, updateGoal] = useState(48000);
+  const [goal, updateGoal] = useState(14500);
   const [haveNow, updateHaveNow] = useState(0);
   const [current, updateCurrent] = useState('');
 
